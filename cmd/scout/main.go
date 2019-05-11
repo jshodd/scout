c5f6033a686565b226b42918b9f917a8659f85d9
Jacob Shodd <jacobshodd@gmail.com>
2019-05-11 13:28:03 -0400

    using cli package

--- cmd/scout/main.go
+++ cmd/scout/main.go
@@ -5,27 +5,41 @@ import (
 //	"github.com/davecgh/go-spew/spew"
 	nmap "github.com/lair-framework/go-nmap"
 	"os"
+	"log"
 	"os/exec"
+	"strings"
 	"strconv"
+	"github.com/urfave/cli"
 )
 
-func main() {
+func main(){
+	app:= cli.NewApp()
+	app.Name = "scout"
+	app.Usage = "scout will kickstart your enumeration process (and do most of it for you)"
+	app.Action = nmapScan
+	err := app.Run(os.Args)
+	if err != nil{
+		log.Fatal(err)
+	}
+}
+
+
+func nmapScan(c *cli.Context) error {
 	var (
 		scan   *nmap.NmapRun
 		cmdOut []byte
 		err    error
 	)
-	host := "10.11.1.5"
+	host := c.Args().Get(0)
 	cmd := "nmap"
-	args := []string{"-sV", "-O", "-sC", "-oX", "-", "-T", "4", host}
+	args := []string{"-sV", "-O", "-p-", "-oX", "-", "-T", "4", host}
+	fmt.Printf("Starting the following scan: %s %s\n",cmd, strings.Join(args[:]," "))
 	if cmdOut, err = exec.Command(cmd, args...).Output(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
 	scan, err = nmap.Parse(cmdOut)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return err
 	}
 	for _,host := range scan.Hosts {
 		for _,address := range host.Addresses {
@@ -37,5 +51,5 @@ func main() {
 			fmt.Println(strconv.Itoa(port.PortId))
 		}
 	}
-
+	return nil
 }
